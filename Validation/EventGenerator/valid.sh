d1b908d3cbb7961000376673fc4b745c20f6d956
Federico Ferri <federico.ferri@cern.ch>
2019-02-01 18:31:10 +0100

    adjust DQM cmsDriver command (input eventcontent and output tiers) and add the conversion from DQM to histos

--- Validation/EventGenerator/valid.sh
+++ Validation/EventGenerator/valid.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-cmsDriver.py VALID --filein file:edm.root --fileout file:dqm.root --mc --eventcontent RAWSIM,LHE --datatier GEN,LHE --conditions 102X_upgrade2018_realistic_v11 --beamspot Realistic25ns13TeVEarly2018Collision --step VALIDATION:genvalid_gj --nThreads 1 --geometry DB:Extended --era Run2_2018 --customise Configuration/DataProcessing/Utils.addMonitoring -n -1
+# working
+cmsDriver.py VALID --filein file:gen.root --fileout file:dqm.root --mc --eventcontent DQM --datatier DQMIO --conditions 102X_upgrade2018_realistic_v11 --beamspot Realistic25ns13TeVEarly2018Collision --step VALIDATION:genvalid_gj --nThreads 1 --geometry DB:Extended --era Run2_2018 --customise Configuration/DataProcessing/Utils.addMonitoring -n -1
 
+DQMIO2histo.py -in dqm.root -o histos.root
